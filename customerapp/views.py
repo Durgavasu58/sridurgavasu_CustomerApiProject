417c9bf8529f8b7cef9d10f1c907b87ba6e9419a
durgavasu <durgavasu58@gmail.com>
2024-03-28 08:33:12 +0000

    added comments

--- customerapp/views.py
+++ customerapp/views.py
@@ -12,26 +12,21 @@ class CustomerRecordViewSet(viewsets.ModelViewSet):
     queryset = CustomerRecord.objects.all()
     serializer_class = CustomerRecordSerializer
 
-    def retrieve(self, request, pk=None):
-        queryset = self.get_queryset()
-        record = queryset.filter(pk=pk).first()
-        if record:
-            serializer = self.get_serializer(record)
-            return Response(serializer.data)
-        else:
-            return Response({"error": "Record not found"}, status=404)
-
     def list_by_phone_number(self, request, phone_number):
+        # filter the customer records with phonenumber 
         queryset = CustomerRecord.objects.filter(phone_number__phone_number=phone_number)
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
 
     def list_by_week(self, request, week):
+
+        # filter the customer records with week 
         queryset = CustomerRecord.objects.filter(week=week)
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
     
     def list_by_month(self, request, month):
+        # filter the customer records with month 
         queryset = CustomerRecord.objects.filter(month=month)
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
